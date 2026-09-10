c11e00bc4412152ea5b1cd661db5218a0130dba2
suleyman-qrs <suleyman.qrs@gmail.com>
2026-09-10 17:24:13 +0200

    i do longer understand why its not working -- will do it another time

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,31 +1,31 @@
-import {rovingIndex} from 'roving-ux'
+import { rovingIndex } from "roving-ux";
 
 rovingIndex({
-  element: document.querySelector('.menu-button'),
-  target: 'a',
-})
+  element: document.querySelector(".menu-button"),
+  target: "a",
+});
 
-const menu = document.querySelector('.menu-button')
-const menuRect = menu.getBoundingClientRect()
+const menu = document.querySelector(".menu-button");
+const menuRect = menu.getBoundingClientRect();
 
-const { matches:motionOK } = window.matchMedia(
-  '(prefers-reduced-motion: no-preference)'
-)
+const { matches: motionOK } = window.matchMedia(
+  "(prefers-reduced-motion: no-preference)",
+);
 
 const getAngles = (clientX, clientY) => {
-  const { x, y, width, height } = menuRect
+  const { x, y, width, height } = menuRect;
 
-  const dx = clientX - (x + 0.5 * width)
-  const dy = clientY - (y + 0.5 * height)
+  const dx = clientX - (x + 0.5 * width);
+  const dy = clientY - (y + 0.5 * height);
 
-  return {dx,dy}
-}
+  return { dx, dy };
+};
 
 if (motionOK) {
-  window.addEventListener('mousemove', ({target, clientX, clientY}) => {
-    const {dx,dy} = getAngles(clientX, clientY)
+  window.addEventListener("mousemove", ({ target, clientX, clientY }) => {
+    const { dx, dy } = getAngles(clientX, clientY);
 
-    menu.attributeStyleMap.set('--x', `${dy / 20}deg`)
-    menu.attributeStyleMap.set('--y', `${dx / 20}deg`)
-  })
+    menu.style.setProperty("--x", `${dy / 20}deg`);
+    menu.style.setProperty("--y", `${dx / 20}deg`);
+  });
 }
