7dd180b0469cc33da13dcf218de2b4a9822200ac
suleyman-qrs <suleyman.qrs@gmail.com>
2026-09-10 15:50:04 +0200

    added seperate files for a menu, and the game, added a roving-uk package and started on the menu

---
+++ src/js/menu.js
@@ -0,0 +1,31 @@
+import {rovingIndex} from 'roving-ux'
+
+rovingIndex({
+  element: document.querySelector('.menu-button'),
+  target: 'a',
+})
+
+const menu = document.querySelector('.menu-button')
+const menuRect = menu.getBoundingClientRect()
+
+const { matches:motionOK } = window.matchMedia(
+  '(prefers-reduced-motion: no-preference)'
+)
+
+const getAngles = (clientX, clientY) => {
+  const { x, y, width, height } = menuRect
+
+  const dx = clientX - (x + 0.5 * width)
+  const dy = clientY - (y + 0.5 * height)
+
+  return {dx,dy}
+}
+
+if (motionOK) {
+  window.addEventListener('mousemove', ({target, clientX, clientY}) => {
+    const {dx,dy} = getAngles(clientX, clientY)
+
+    menu.attributeStyleMap.set('--x', `${dy / 20}deg`)
+    menu.attributeStyleMap.set('--y', `${dx / 20}deg`)
+  })
+}
